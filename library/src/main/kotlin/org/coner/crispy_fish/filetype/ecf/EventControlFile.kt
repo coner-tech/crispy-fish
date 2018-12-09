6a9f72b11d59597b42332eca201c5cf974a52e3a
Carlton Whitehead <cebesius86@gmail.com>
2018-12-09 16:53:50 -0500

    Convert NotEventControlFilePathException to Kotlin

--- library/src/main/kotlin/org/coner/crispy_fish/filetype/ecf/EventControlFile.kt
+++ library/src/main/kotlin/org/coner/crispy_fish/filetype/ecf/EventControlFile.kt
@@ -1,6 +1,5 @@
 package org.coner.crispy_fish.filetype.ecf
 
-import org.crispy_fish.filetype.ecf.NotEventControlFilePathException
 import java.nio.file.Path
 
 class EventControlFile(val path: Path, val isTwoDayEvent: Boolean, val conePenalty: Int) {
