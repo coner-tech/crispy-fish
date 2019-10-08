b0af3b761868d85b0538a183c1d2c6560488be94
Carlton Whitehead <cebesius86@gmail.com>
2019-10-07 20:25:26 -0400

    Cone Killer (#33)
    * THSCC-style Cone Killer query

---
+++ library/src/main/kotlin/org/coner/crispyfish/model/ThsccConeKillerResult.kt
@@ -0,0 +1,8 @@
+package org.coner.crispyfish.model
+
+class ThsccConeKillerResult(
+        val position: Int,
+        val registration: Registration,
+        val conedRuns: List<RegistrationRun>
+) {
+}
---
+++ library/src/test/kotlin/org/coner/crispyfish/model/ThsccConeKillerAssertions.kt
@@ -0,0 +1,19 @@
+package org.coner.crispyfish.model
+
+import assertk.Assert
+import assertk.all
+import assertk.assertions.isEqualTo
+import assertk.assertions.prop
+
+
+fun Assert<ThsccConeKillerResult>.hasPosition(expected: Int) {
+    prop("position", ThsccConeKillerResult::position).isEqualTo(expected)
+}
+
+fun Assert<ThsccConeKillerResult>.registration(body: Assert<Registration>.() -> Unit) {
+    prop("registration", ThsccConeKillerResult::registration).all(body)
+}
+
+fun Assert<ThsccConeKillerResult>.conedRuns(body: Assert<List<RegistrationRun>>.() -> Unit) {
+    prop("conedRuns", ThsccConeKillerResult::conedRuns).all(body)
+}
