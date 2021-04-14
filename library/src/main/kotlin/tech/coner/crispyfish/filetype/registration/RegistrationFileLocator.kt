71c0182465a57d0fd5666338c31768bd66ed2dcf
Carlton Whitehead <cebesius86@gmail.com>
2021-04-14 08:22:29 -0400

    Simplify RegistrationFileLocator
    Not interested in the incrementing backup files

--- library/src/main/kotlin/tech/coner/crispyfish/filetype/registration/RegistrationFileLocator.kt
+++ library/src/main/kotlin/tech/coner/crispyfish/filetype/registration/RegistrationFileLocator.kt
@@ -9,21 +9,7 @@ class RegistrationFileLocator(val eventControlFile: EventControlFile) {
 
     fun locate(): RegistrationFile {
         val folder = eventControlFile.file.parentFile
-        val file = folder.listFiles(filter)
-                .sortedByDescending(File::lastModified)
-                .first()
+        val file = folder.resolve("${eventControlFile.file.nameWithoutExtension}.rgg")
         return RegistrationFile(file)
-    }
-
-    private val ecfFileNameWithoutExtension by lazy { eventControlFile.file.nameWithoutExtension }
-    private val incrementingRegistrationFilesSuffix = Pattern.compile(
-            "^\\Q${ecfFileNameWithoutExtension}_rgg\\E\\.[0-9]{3}\$",
-            Pattern.CASE_INSENSITIVE
-    )
-
-    private val filter = FileFilter { file ->
-        return@FileFilter if (!file.name.startsWith(ecfFileNameWithoutExtension, ignoreCase = true)) false
-        else if (!file.extension.equals("rgg", ignoreCase = true) && !incrementingRegistrationFilesSuffix.matcher(file.name).matches()) false
-        else true
     }
 }
