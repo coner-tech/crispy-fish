b0bf2c0414bf2674e50d8b7eae827969bbda6664
Carlton Whitehead <cebesius86@gmail.com>
2023-08-09 20:18:10 -0400

    Fix various inspection issues and clean up StagingFilenames

--- library/src/main/kotlin/tech/coner/crispyfish/filetype/staging/StagingFilenames.kt
+++ library/src/main/kotlin/tech/coner/crispyfish/filetype/staging/StagingFilenames.kt
@@ -4,10 +4,10 @@ import java.util.regex.Pattern
 
 object StagingFilenames {
 
-    val ORIGINAL_FILE_DAY_1 = Pattern.compile(".*.st1$")
-    val ORIGINAL_FILE_DAY_2 = Pattern.compile(".*.st2$")
-    val ORIGINAL_FILE_DAY_1_EXTENSION = "st1"
-    val ORIGINAL_FILE_DAY_2_EXTENSION = "st2"
+    val ORIGINAL_FILE_DAY_1: Pattern = Pattern.compile(".*.st1$")
+    val ORIGINAL_FILE_DAY_2: Pattern = Pattern.compile(".*.st2$")
+    const val ORIGINAL_FILE_DAY_1_EXTENSION = "st1"
+    const val ORIGINAL_FILE_DAY_2_EXTENSION = "st2"
     val ORIGINAL_FILE_EXTENSIONS = arrayOf(ORIGINAL_FILE_DAY_1_EXTENSION, ORIGINAL_FILE_DAY_2_EXTENSION)
 
 }
